398fe58f710b7c0a43379f16d862605e35d69c9d
ronaldoaf <ronaldoaf>
2016-10-13 14:58:10 -0300

    atualiza

--- www/js/index.js
+++ www/js/index.js
@@ -16,6 +16,25 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+ 
+ 
+ var scheduleDelayed = function () {
+	var now = new Date().getTime(),
+		_5_sec_from_now = new Date(now + 5 * 1000);
+
+	var sound = device.platform == 'Android' ? 'file://sound.mp3' : 'file://beep.caf';
+
+	cordova.plugins.notification.local.schedule({
+		id: 1,
+		title: 'Scheduled with delay',
+		text: 'Test Message 1',
+		at: _5_sec_from_now,
+		sound: sound,
+		badge: 12
+	});
+};
+ 
+ 
 var app = {
     // Application Constructor
     initialize: function() {
@@ -45,5 +64,9 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+		
+		
+		
+		
     }
 };
