3bde369d12035ef9fab33f24c55e828937c82893
ronaldoaf <ronaldoaf>
2016-10-13 15:39:20 -0300

    atualiza

--- www/js/index.js
+++ www/js/index.js
@@ -26,13 +26,28 @@
 
 	cordova.plugins.notification.local.schedule({
 		id: 1,
-		title: 'Scheduled with delay',
-		text: 'Test Message 1',
+		title: 'Alerta',
+		text: 'Isso é um alerta',
 		at: _5_sec_from_now,
 		sound: sound,
 		badge: 12
 	});
+	
+	cordova.plugins.notification.local.on("trigger", function (notification) {
+        if (notification.id != 1) return;
+		
+		navigator.vibrate(5000);
+		
+    });
+	
+	
+	
+	
 };
+ 
+
+ 
+ 
  
  
 var app = {
